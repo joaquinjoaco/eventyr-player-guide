9716fafa17114431dddeecbe819a40a8efa9e829
Joaquin <96495159+joaquinjoaco@users.noreply.github.com>
2022-09-21 19:03:50 -0300

    added a navbar

---
+++ scripts/sticky.js
@@ -0,0 +1,6 @@
+// Sticky navbar
+window.addEventListener("scroll", function () {
+     var header = document.querySelector("header");
+     header.classList.toggle("sticky", window.scrollY > 0);
+     // }
+})
