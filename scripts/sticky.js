7f8dd80f282e3306130d6a90bc4addb3e537965a
Joaquin <96495159+joaquinjoaco@users.noreply.github.com>
2022-09-22 01:04:22 -0300

    Removed JS stickiness
    Last tweaks before going to bed.
    Removed the nav stickiness performed with JS, and replaced it with just position:fixed css.
    Because of this change I also removed that black magic done with the div used to help the website not have a seizure.

--- scripts/sticky.js
+++
@@ -1,6 +0,0 @@
-// Sticky navbar
-window.addEventListener("scroll", function () {
-     var header = document.querySelector("header");
-     header.classList.toggle("sticky", window.scrollY > 0);
-     // }
-})
